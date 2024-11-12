29a4e2ce5aeb5bff00a99cf3c3a7cd4c5a32c93f
natsu0317 <mineno-natsuko686@g.ecc.u-tokyo.ac.jp>
2024-11-12 17:10:56 +0900

    direct mapped cache

--- source/cache/direct.c
+++ source/cache/direct.c
@@ -1,111 +1,159 @@
 #include <stdio.h>
-#include <math.h>
 #include <stdint.h>
 #include <string.h>
 #include <stdlib.h>
+#include <time.h>
 
-#define MAX_INSTRUCTION 1000
+#define CACHE_LINES 1024
+#define LINE_SIZE 16  // 128ビット = 16バイト
+#define MEMORY_SIZE 65536  // より大きなメモリサイズを使用
 
-#define CACHE_LINE 1024
-#define LINE_SIZE 128
-#define MEMORY_SIZE 4096
-
-
-struct cache_line{
+struct cache_line {
     char valid;
     uint32_t tag;
-    char data[LINE_SIZE];
+    uint8_t data[LINE_SIZE];
 };
 
-struct cache_line cache[CACHE_LINE];
-char main_memory[MEMORY_SIZE];
+struct cache_line cache[CACHE_LINES];
+uint8_t main_memory[MEMORY_SIZE];
 
-void init_cache(){
-    for(int i=0; i<LINE_SIZE; i++){
+int hit_count = 0;
+int miss_count = 0;
+
+void init_cache() {
+    for (int i = 0; i < CACHE_LINES; i++) {
         cache[i].tag = 0;
         cache[i].valid = 0;
         memset(cache[i].data, 0, LINE_SIZE);
     }
 }
 
-void init_memory(){
+void init_memory() {
     for (int i = 0; i < MEMORY_SIZE; i++) {
-        main_memory[i] = rand() % 256;  // 0-255のランダムな値で初期化
+        main_memory[i] = rand() % 256;
     }
 }
 
-void read_from_memory(uint32_t address, char* data, int size) {
-    memcpy(data, &main_memory[address], size);
+int cache_read(uint32_t address, uint8_t* data, int size) {
+    uint32_t tag = address / (CACHE_LINES * LINE_SIZE);
+    uint32_t index = (address / LINE_SIZE) % CACHE_LINES;
+    uint32_t offset = address % LINE_SIZE;
+
+    if (cache[index].valid && cache[index].tag == tag) {
+        printf("Cache Hit at address 0x%x\n", address);
+        memcpy(data, &cache[index].data[offset], size);
+        hit_count++;
+        return 1;  // Cache hit
+    } else {
+        printf("Cache Miss at address 0x%x\n", address);
+        uint32_t block_start = address - offset;
+        memcpy(cache[index].data, &main_memory[block_start], LINE_SIZE);
+        cache[index].tag = tag;
+        cache[index].valid = 1;
+        memcpy(data, &cache[index].data[offset], size);
+        miss_count++;
+        return 0;  // Cache miss
+    }
 }
 
-void write_to_memory(uint32_t address, const char* data, int size) {
+int cache_write(uint32_t address, const uint8_t* data, int size) {
+    uint32_t tag = address / (CACHE_LINES * LINE_SIZE);
+    uint32_t index = (address / LINE_SIZE) % CACHE_LINES;
+    uint32_t offset = address % LINE_SIZE;
+
+    if (cache[index].valid && cache[index].tag == tag) {
+        printf("Cache Hit (Write) at address 0x%x\n", address);
+        hit_count++;
+    } else {
+        printf("Cache Miss (Write) at address 0x%x\n", address);
+        uint32_t block_start = address - offset;
+        memcpy(cache[index].data, &main_memory[block_start], LINE_SIZE);
+        cache[index].tag = tag;
+        cache[index].valid = 1;
+        miss_count++;
+    }
+
+    // Write-through policy
+    memcpy(&cache[index].data[offset], data, size);
     memcpy(&main_memory[address], data, size);
+    return cache[index].valid && cache[index].tag == tag;
 }
 
-void cache_read(uint32_t address, char*data, int size){
-    int cache_line = log2(CACHE_LINE);
-    int line_size = log2(LINE_SIZE);
-    uint32_t tag = (address >> (line_size + cache_line)) & 0x7FFF;
-    uint32_t index = (address >> line_size) & 0x3FF;
-    uint32_t offset = address & 0x7F;
+void print_cache_line(uint32_t index) {
+    printf("Cache Line %u: Valid=%d, Tag=0x%x, Data=", index, cache[index].valid, cache[index].tag);
+    for (int i = 0; i < LINE_SIZE; i++) {
+        printf("%02x ", cache[index].data[i]);
+    }
+    printf("\n");
+}
 
-    if(cache[index].valid && cache[index].tag == tag){
-        printf("Hit\n");
-        memcpy(data, &cache[index].data[offset], size);
-    } else {
-        printf("Miss\n");
-        read_from_memory(address - offset, cache[index].data, LINE_SIZE);
-        cache[index].tag = tag;
-        cache[index].valid = 1;
-        
-        memcpy(data, &cache[index].data[offset], size);
+void print_stats() {
+    int total_accesses = hit_count + miss_count;
+    printf("\nCache Statistics:\n");
+    printf("Total accesses: %d\n", total_accesses);
+    printf("Hits: %d (%.2f%%)\n", hit_count, (float)hit_count / total_accesses * 100);
+    printf("Misses: %d (%.2f%%)\n", miss_count, (float)miss_count / total_accesses * 100);
+}
+
+void sequential_access_test(int num_accesses) {
+    printf("\nSequential Access Test (%d accesses):\n", num_accesses);
+    uint8_t data[8];
+    for (int i = 0; i < num_accesses; i++) {
+        cache_read(i * 8, data, 8);
     }
 }
 
-int cache_write(uint32_t address, const char* data, int size){
-    int cache_line = log2(CACHE_LINE);
-    int line_size = log2(LINE_SIZE);
-    uint32_t tag = (address >> (line_size + cache_line)) & 0x7FFF;
-    uint32_t index = (address >> line_size) & 0x3FF;
-    uint32_t offset = address & 0x7F;
-    
-    if(cache[index].valid && cache[index].tag == tag){
-        printf("Hit\n");
-        memcpy(&cache[index].data[offset], data, size);
-        write_to_memory(address, data, size);
-        return 1;
-    } else {
-        printf("Miss\n");
-        read_from_memory(address - offset, cache[index].data, LINE_SIZE);
-        cache[index].tag = tag;
-        cache[index].valid = 1;
-        memcpy(&cache[index].data[offset], data, size);
-        write_to_memory(address, data, size);
-        return 0; // Cache miss
+void random_access_test(int num_accesses) {
+    printf("\nRandom Access Test (%d accesses):\n", num_accesses);
+    uint8_t data[8];
+    for (int i = 0; i < num_accesses; i++) {
+        uint32_t address = rand() % (MEMORY_SIZE - 8);
+        cache_read(address, data, 8);
     }
-
 }
 
+void locality_test(int num_accesses, int locality_range) {
+    printf("\nLocality Test (%d accesses, range %d):\n", num_accesses, locality_range);
+    uint8_t data[8];
+    uint32_t base_address = rand() % (MEMORY_SIZE - locality_range);
+    for (int i = 0; i < num_accesses; i++) {
+        uint32_t offset = rand() % locality_range;
+        cache_read(base_address + offset, data, 8);
+    }
+}
 
-int main(){
-    init_cache();    
+int main() {
+    srand(time(NULL));
+    init_cache();
+    init_memory();
 
-    char data[8];
-    
+    // Basic tests
+    uint8_t data[8];
     cache_read(0, data, 8);
-    cache_read(128, data, 8);
+    cache_read(16, data, 8);
     cache_read(1024, data, 8);
     cache_read(0, data, 8);
-    
-    char write_data[8] = {1, 2, 3, 4, 5, 6, 7, 8};
+
+    uint8_t write_data[8] = {1, 2, 3, 4, 5, 6, 7, 8};
     cache_write(256, write_data, 8);
     cache_read(256, data, 8);
-    
+
     printf("Data read from address 256: ");
     for (int i = 0; i < 8; i++) {
         printf("%d ", data[i]);
     }
     printf("\n");
-    
+
+    print_cache_line(0);
+    print_cache_line(1);
+    print_cache_line(16);
+
+    // Complex tests
+    sequential_access_test(1000);
+    random_access_test(1000);
+    locality_test(1000, 1024);
+
+    print_stats();
+
     return 0;
 }
