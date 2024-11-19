09e7f343ceaec540699b1f2cba280441a1e88fe8
natsu0317 <mineno-natsuko686@g.ecc.u-tokyo.ac.jp>
2024-11-19 15:01:33 +0900

    fix

--- source/binary_to_result/binary_to_result.h
+++ source/binary_to_result/binary_to_result.h
@@ -1,8 +1,15 @@
 #ifndef BITORESULT_H
 #define BITORESULT_H
 
+typedef struct{
+    int pc;
+    int opcode; //sw,lwなら1,分岐命令なら2,それ以外なら0
+    int operand1;
+    int operand2;
+    int operand3;
+}Pc_operand;
 
-int execute_binary_instruction(const char binary_instruction[][33], int num_instructions, int current_line);
+Pc_operand execute_binary_instruction(const char binary_instruction[][33], int num_instructions, int current_line);
 int result_main();
 void print_register_transition(FILE *transition_file, int pc);
 void for_markdown(FILE *transition_file);
