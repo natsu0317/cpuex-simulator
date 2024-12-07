550cd8f5deb758f2af509f17f52ea50a8bdae8db
natsu0317 <mineno-natsuko686@g.ecc.u-tokyo.ac.jp>
2024-12-07 16:29:10 +0900

    csrw修正

--- source/asm_to_binary/parser.c
+++ source/asm_to_binary/parser.c
@@ -600,7 +600,7 @@ void parse_assembly(const char* assembly_code){
         }
         if(is_c_type(opcode)){
             if(strcmp(opcode, "csrr") == 0) snprintf(inst.binary_code, sizeof(inst.binary_code),"00000000000000000000100010101010");
-            if(strcmp(opcode, "csrw") == 0) snprintf(inst.binary_code, sizeof(inst.binary_code),"00000000000000010100010000001010");
+            if(strcmp(opcode, "csrw") == 0) snprintf(inst.binary_code, sizeof(inst.binary_code),"00000000101000000000010000001010");
         }
         //printf("Binary Code: %s\n",inst.binary_code);
         ////printf("before");
