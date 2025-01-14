f5f4c79eafb29f7dc4139b4693aa0dc2ccc3596d
natsu0317 <mineno-natsuko686@g.ecc.u-tokyo.ac.jp>
2025-01-14 12:21:39 +0900

    b系列命令コード整理

--- source/binary_to_result/parser.c
+++ source/binary_to_result/parser.c
@@ -270,94 +270,48 @@ Pc_operand execute_binary_instruction(const char binary_instruction[][33], const
                     pc_operand.opcode = 2; 
                     pc_operand.operand2 = rs1;
                     pc_operand.operand3 = rs2;
-                   //printf("12; %d, 11: %d, 10_5: %d, 4_1: %d\n",bit12,bit11,bit10_5,bit4_1);
-                   //printf("funct3:%d\n",funct3);
                     imm |= (bit12 << 12);
                     imm |= (bit11 << 11);
                     imm |= (bit10_5 << 5);
                     imm |= (bit4_1 << 1);
-                   //printf("imm:%d\n",imm);
                     if(bit12 == 1){
                         //immは負の値
-                        //2の補数
                         uint32_t mask = (1 << 12) -1;
                         imm = ~imm & mask;
                         imm = imm + 1;
-                       //printf("imm:%d\n",imm);
-                        if (funct3 == 0) {  // beq
-                            if(get_register(rs1) == get_register(rs2)){
-                               //printf("beq: x%d, x%d, -%d\n", rs1, rs2, imm);
-                               counter.b_type[0]++;
-                                pc -= imm/4;
-                            }
-                        } else if(funct3 == 0x1){  // bne
-                            if(get_register(rs1) != get_register(rs2)){
-                               //printf("bne: x%d, x%d, -%d\n", rs1, rs2, imm);
-                               counter.b_type[1]++;
-                               //printf("pc:%d\n",pc);
-                                pc -= imm/4;
-                               //printf("after_pc:%d\n",pc);
-                            }
-                        } else if(funct3 == 0x4){  // blt
-                            if(get_register(rs1) < get_register(rs2)){
-                               //printf("blt: x%d, x%d, -%d\n", rs1, rs2, imm);
-                               counter.b_type[2]++;
-                                pc -= imm/4;
-                            }
-                        } else if(funct3 == 0x5){  // bge
-                            if(get_register(rs1) >= get_register(rs2)){
-                               //printf("bge: x%d, x%d, -%d\n", rs1, rs2, imm);
-                               counter.b_type[3]++;
-                                pc -= imm/4;
-                            }
-                        } else if(funct3 == 0x6){  // bgt
-                            if(get_register(rs1) > get_register(rs2)){
-                               //printf("bgt: x%d, x%d, -%d\n", rs1, rs2, imm);
-                               counter.b_type[3]++;
-                                pc -= imm/4;
-                            }
-                        }  else {
-                            pc = 1;
-                        }
-                    } else if (funct3 == 0) {  // beq
-                       //printf("beq\n");
-                        if(get_register(rs1) == get_register(rs2)){
-                           //printf("beq: x%d, x%d, %d\n", rs1, rs2, imm);
-                           counter.b_type[0]++;
-                            pc += imm/4;
-                        }
-                    } else if(funct3 == 0x1){  // bne
-                        if(get_register(rs1) != get_register(rs2)){
-                           //printf("bne: x%d, x%d, %d\n", rs1, rs2, imm);
-                           counter.b_type[1]++;
-                            //printf("pc:%d\n",pc);
-                            pc += imm/4;
-                            //printf("after_pc:%d\n",pc);
-                        }
-                    } else if(funct3 == 0x4){  // blt
-                        if(get_register(rs1) < get_register(rs2)){
-                           //printf("blt: x%d, x%d, %d\n", rs1, rs2, imm);
-                           counter.b_type[2]++;
-                            pc += imm/4;
-                        }
-                    } else if(funct3 == 0x5){  // bge
-                        //printf("bge");
-                        //printf("rs1: %d, rs2: %d\n",get_register(rs1),get_register(rs2));
-                        counter.b_type[3]++;
-                        if(get_register(rs1) >= get_register(rs2)){
-                           //printf("bge: x%d, x%d, %d\n", rs1, rs2, imm);
-                            pc += imm/4;
-                        }
-                    } else if(funct3 == 0x6){  // bgt
-                        //printf("bgt");
-                        //printf("rs1: %d, rs2: %d\n",get_register(rs1),get_register(rs2));
-                        counter.b_type[3]++;
-                        if(get_register(rs1) > get_register(rs2)){
-                           //printf("bgt: x%d, x%d, %d\n", rs1, rs2, imm);
-                            pc += imm/4;
-                        }
+                        imm = -imm;
                     }
-                    //printf("pc: 361: %d\n",pc);
+                    int rs1_value = get_register(rs1);
+                    int rs2_value = get_register(rs2);
+                    int should_branch = 0;
+                    switch(funct3){
+                        case 0x0: //beq
+                            should_branch = (rs1_value == rs2_value);
+                            counter.b_type[0]++;
+                            break;
+                        case 0x1: //bne
+                            should_branch = (rs1_value != rs2_value);
+                            counter.b_type[1]++;
+                            break;
+                        case 0x4: //blt
+                            should_branch = (rs1_value < rs2_value);
+                            counter.b_type[2]++;
+                            break;
+                        case 0x5:  // bge
+                            should_branch = (get_register(rs1) >= get_register(rs2));
+                            counter.b_type[3]++;
+                            break;
+                        case 0x6:  // bgt
+                            should_branch = (get_register(rs1) > get_register(rs2));
+                            counter.b_type[4]++;
+                            break;
+                        default:
+                            break;
+                    }
+
+                    if(should_branch){ //分岐実行
+                        pc += imm/4;
+                    }
                     if(pc == 0){
                         pc = 1;
                     }
