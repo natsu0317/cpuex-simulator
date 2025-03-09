b83f61b187de82edaafabed189a1b45cfd139d15
natsu0317 <mineno-natsuko686@g.ecc.u-tokyo.ac.jp>
2025-03-10 03:36:14 +0900

    fpu

--- source/fast_parser/fast_parser.c
+++ source/fast_parser/fast_parser.c
@@ -640,279 +640,31 @@ int handle_lw(uint32_t instruction, uint32_t rd, uint32_t rs1, int current_line)
     return 1;
 }
 
-void print_float_as_32bit(FILE* file, float value) {
-    uint32_t bits;
-    memcpy(&bits, &value, sizeof(bits));
-    for (int i = 31; i >= 0; i--) {
-        fprintf(file, "%c", ((bits >> i) & 1) ? '1' : '0');
-    }
-}
-
-uint32_t fpu_fadd(float a, float b){
-    uint32_t a_bits, b_bits;
-    memcpy(&a_bits, &a, sizeof(a_bits));
-    memcpy(&b_bits, &b, sizeof(b_bits));
-
-    uint32_t s1 = (a_bits >> 31) & 0x1;
-    uint32_t s2 = (b_bits >> 31) & 0x1;
-    uint32_t e1 = (a_bits >> 23) & 0xFF;
-    uint32_t e2 = (b_bits >> 23) & 0xFF;
-    uint32_t m1 = a_bits & 0x7FFFFF;
-    uint32_t m2 = b_bits & 0x7FFFFF;
-
-    // printf("s1: %x, e1: %x, m1: %x\n",s1, e1, m1);
-    // printf("s2: %x, e2: %x, m2: %x\n",s2, e2, m2);
-
-    int32_t m1a, m2a;
-    int16_t e1a, e2a;
-    if (e1 == 0) {
-        m1a = m1;
-        e1a = 1;
-    } else {
-        m1a = m1 | 0x800000;
-        e1a = e1;
-    }
-    if (e2 == 0) {
-        m2a = m2;
-        e2a = 1;
-    } else {
-        m2a = m2 | 0x800000;
-        e2a = e2;
-    }
-
-    int ce;
-    int16_t tde;
-    if (e1a > e2a) {
-        ce = 0;
-        tde = e1a - e2a;
-    } else {
-        ce = 1;
-        tde = e2a - e1a;
-    }
-
-    int32_t de;
-    if (tde > 31) {
-        de = 31;
-    } else {
-        de = tde & 0x1F;
-    }
-
-    int sel = ce;
-    if (de == 0) {
-        if (m1a > m2a) {
-            sel = 0;
-        } else {
-            sel = 1;
-        }
-    }
-
-    uint32_t ms;
-    uint64_t mi;
-    uint16_t es;
-    int ss;
-    if (sel == 0) {
-        ms = m1a;
-        mi = m2a;
-        es = e1a;
-        ss = s1;
-    } else {
-        ms = m2a;
-        mi = m1a;
-        es = e2a;
-        ss = s2;
-    }
-
-    uint64_t mia = mi << 31;
-    mia = mia >> de;
-    int tstck = 0;
-    if ((mia & 0x1FFFFFFF) != 0) {
-        tstck = 1;
-    }
-
-    uint32_t mye;
-    if (s1 == s2) {
-        mye = (ms << 2) + (mia >> 29);
-    } else {
-        mye = (ms << 2) - (mia >> 29);
-    }
-
-    uint16_t esi = es + 1;
-
-    uint32_t myd;
-    uint16_t eyd;
-    int stck;
-    if (((mye >> 26) & 0x1) == 0) {
-        myd = mye;
-        eyd = es;
-        stck = tstck;
-    } else {
-        eyd = esi;
-        if (esi == 0xFF) {
-            myd = 1 << 25;
-            stck = 0;
-        } else {
-            myd = mye >> 1;
-            stck = tstck || (mye & 0x1);
-        }
-    }
-
-    uint16_t se;
-    if (((myd >> 25) & 0x1) != 0) {
-        se = 0;
-    } else if (((myd >> 24) & 0x1) != 0) {
-        se = 1;
-    } else if (((myd >> 23) & 0x1) != 0) {
-        se = 2;
-    } else if (((myd >> 22) & 0x1) != 0) {
-        se = 3;
-    } else if (((myd >> 21) & 0x1) != 0) {
-        se = 4;
-    } else if (((myd >> 20) & 0x1) != 0) {
-        se = 5;
-    } else if (((myd >> 19) & 0x1) != 0) {
-        se = 6;
-    } else if (((myd >> 18) & 0x1) != 0) {
-        se = 7;
-    } else if (((myd >> 17) & 0x1) != 0) {
-        se = 8;
-    } else if (((myd >> 16) & 0x1) != 0) {
-        se = 9;
-    } else if (((myd >> 15) & 0x1) != 0) {
-        se = 10;
-    } else if (((myd >> 14) & 0x1) != 0) {
-        se = 11;
-    } else if (((myd >> 13) & 0x1) != 0) {
-        se = 12;
-    } else if (((myd >> 12) & 0x1) != 0) {
-        se = 13;
-    } else if (((myd >> 11) & 0x1) != 0) {
-        se = 14;
-    } else if (((myd >> 10) & 0x1) != 0) {
-        se = 15;
-    } else if (((myd >> 9) & 0x1) != 0) {
-        se = 16;
-    } else if (((myd >> 8) & 0x1) != 0) {
-        se = 17;
-    } else if (((myd >> 7) & 0x1) != 0) {
-        se = 18;
-    } else if (((myd >> 6) & 0x1) != 0) {
-        se = 19;
-    } else if (((myd >> 5) & 0x1) != 0) {
-        se = 20;
-    } else if (((myd >> 4) & 0x1) != 0) {
-        se = 21;
-    } else if (((myd >> 3) & 0x1) != 0) {
-        se = 22;
-    } else if (((myd >> 2) & 0x1) != 0) {
-        se = 23;
-    } else if (((myd >> 1) & 0x1) != 0) {
-        se = 24;
-    } else if ((myd & 0x1) != 0) {
-        se = 25;
-    } else {
-        se = 26;
-    }
-
-    uint32_t myf_f, myf_s;
-    myf_f = myd << se;
-    myf_s = myd << ((eyd - 1) & 0x1F);
-
-    uint16_t eyf = eyd - se;
-    uint16_t eyr;
-    uint32_t myf;
-    if (eyd > se) {
-        eyr = eyf & 0xFF;
-        myf = myf_f;
-    } else {
-        eyr = 0;
-        myf = myf_s;
-    }
-
-    uint32_t myr;
-    if (((myf & 0x2) != 0 && (myf & 0x1) == 0 && stck == 0 && (myf & 0x4) != 0) ||
-        ((myf & 0x2) != 0 && (myf & 0x1) == 0 && s1 == s2 && stck == 1) ||
-        ((myf & 0x2) != 0 && (myf & 0x1) != 0)) {
-        myr = (myf >> 2) + 1;
-    } else {
-        myr = myf >> 2;
-    }
-
-    uint16_t eyri, ey;
-    uint32_t my;
-    eyri = eyr + 1;
-    if (((myr >> 24) & 0x1) != 0) {
-        ey = eyri;
-        my = 0;
-    } else if ((myr & 0xFFFFFF) == 0) {
-        ey = 0;
-        my = 0;
-    } else {
-        ey = eyr;
-        my = myr & 0x7FFFFF;
-    }
-
-    int sy;
-    if (ey == 0 && my == 0) {
-        sy = s1 && s2;
-    } else {
-        sy = ss;
-    }
-
-    // 結果を構成
-    uint32_t result_raw;
-    if (e1 == 0 && e2 == 0) {
-        result_raw = 0;
-    } else if (e1 == 0) {
-        result_raw = b_bits;
-    } else if (e2 == 0) {
-        result_raw = a_bits;
-    } else {
-        result_raw = (sy << 31) | ((ey & 0xFF) << 23) | (my & 0x7FFFFF);
-    }
-    
-    return result_raw;
-
-}
-
 int handle_f(uint32_t instruction, uint32_t rd, uint32_t rs1, uint32_t rs2, uint32_t func3, FILE* log_file, FILE* memory_file){
     uint32_t func7 = (instruction >> 27) & 0x1F;
     float a1 = get_float_register(rs1);
     float a2 = get_float_register(rs2);
     float result;
     if(func7 == 0){
-        uint32_t value = fpu_fadd(a1,a2);
-        memcpy(&result, &value, sizeof(uint32_t));
-        fprintf(log_file, "fadd %.9f, %.9f = ", a1, a2);
-        print_float_as_32bit(log_file, result);
-        fprintf(log_file, "\n");
+        result = fadd(a1,a2);
         set_register(rd, result);
         // counter.f_type[0]++;
         total_stall += 4;
     }
     if(func7 == 1){
-        uint32_t value = fpu_fadd(a1,-a2);
-        memcpy(&result, &value, sizeof(uint32_t));
-        fprintf(log_file, "fsub %.9f, %.9f = ", a1, a2);
-        print_float_as_32bit(log_file, result);
-        fprintf(log_file, "\n");
+        result = fadd(a1,-a2);
         set_register(rd, result);
         // counter.f_type[1]++;
         total_stall += 4;
     }
     if(func7 == 2){
         result = fmul(a1,a2);
-        fprintf(log_file, "fmul %.9f, %.9f = ", a1, a2);
-        print_float_as_32bit(log_file, result);
-        fprintf(log_file, "\n");
         set_register(rd, result);
         // counter.f_type[2]++;
         total_stall += 2;
     }
     if(func7 == 3){
         result = fdiv(a1,a2);
-        fprintf(log_file, "fdiv %.9f, %.9f = %.9f", a1, a2,result);
-        print_float_as_32bit(log_file, result);
-        fprintf(log_file, "\n");
         set_register(rd, result);
         // counter.f_type[3]++;
         total_stall += 11;
@@ -920,66 +672,36 @@ int handle_f(uint32_t instruction, uint32_t rd, uint32_t rs1, uint32_t rs2, uint
     if(func7 == 4){
         if(func3 == 1){// fsgnjn
             result = fsgnjn(a1,a2);
-            fprintf(log_file, "fsgnjn %.9f, %.9f = ", a1, a2);
-            print_float_as_32bit(log_file, result);
-            fprintf(log_file, "\n");
             set_register(rd, result);
             // counter.f_type[8]++;
         }
         if(func3 == 2){// fsgnjx
             result = fsgnjx(a1,a2);
-            fprintf(log_file, "fsgnjx %.9f, %.9f = ", a1, a2);
-            print_float_as_32bit(log_file, result);
-            fprintf(log_file, "\n");
             set_register(rd, result);
             // counter.f_type[9]++;
         }
     }
     if(func7 == 12){
         result = fabsf(a1);
-        fprintf(log_file, "fabsf %.9f = ", a1);
-        print_float_as_32bit(log_file, result);
-        fprintf(log_file, "\n");
         set_register(rd, result);
         // counter.f_type[4]++;
     }
     if(func7 == 13){
         result = fsgnjn(a1,a1);
-        fprintf(log_file, "fneg %.9f = ", a1);
-        print_float_as_32bit(log_file, result);
-        fprintf(log_file, "\n");
         set_register(rd, result);
         // counter.f_type[5]++;
     }
-    // if(func7 == 10){
-    //     result = finv(a1);
-    //     // fprintf(log_file, "finv %.9f = ", a1);
-    //     // print_float_as_32bit(log_file, result);
-    //     // fprintf(log_file, "\n");
-    //     set_register(rd, result);
-    //     // counter.f_type[6]++;
-    // }
     if(func7 == 11){
         result = fsqrts(a1);
-        fprintf(log_file, "fsqrt %.9f = ", a1);
-        print_float_as_32bit(log_file, result);
-        fprintf(log_file, "\n");
-        // printf("%.2f\t%.6f\t%.6f\t%.2e\n", a1, result, sqrtf(a1), result-sqrtf(a1));
-        // result = sqrtf(a1);
         set_register(rd, result);
         // counter.f_type[7]++;
         total_stall += 8;
     }
     if(func7 == 20){
         if(func3 == 1){//flt
-            //printf("flt x%d, x%d, x%d\n", rd, r1, r2);
             bool comparison_result = flt(a1, a2);
             result = (double)comparison_result; // boolをdoubleに変換
-            fprintf(log_file, "flt %.9f, %.9f = ", a1, a2);
-            // 比較結果をfloatとして扱い、32ビットで出力
             float float_result = (float)comparison_result;
-            print_float_as_32bit(log_file, float_result);
-            fprintf(log_file, "\n");
             //printf("result: %f\n", result); // %fを使用してdoubleを表示
             set_register(rd, result);
             // counter.f_type[11]++;
@@ -987,11 +709,7 @@ int handle_f(uint32_t instruction, uint32_t rd, uint32_t rs1, uint32_t rs2, uint
         if(func3 == 2){//feq
             bool comparison_result = feq(a1, a2);
             result = (float)comparison_result;
-            fprintf(log_file, "flt %.9f, %.9f = ", a1, a2);
-            // 比較結果をfloatとして扱い、32ビットで出力
             float float_result = (float)comparison_result;
-            print_float_as_32bit(log_file, float_result);
-            fprintf(log_file, "\n");
             //printf("result: %f\n", result); // %fを使用してdoubleを表示
             set_register(rd, result);
             // counter.f_type[10]++;
@@ -1001,9 +719,6 @@ int handle_f(uint32_t instruction, uint32_t rd, uint32_t rs1, uint32_t rs2, uint
         // printf("a1:%f\n",a1);
         int32_t int_result = fcvtws(a1); // int32_t 型で結果を受け取る
         result = (double)int_result;     // double 型にキャスト
-        
-        fprintf(log_file, "fcvtws %.9f = %d\n", a1, int_result);
-        // printf("result: %f\n", result);  // %f を使用して double を表示
         set_register(rd, result);
         // counter.f_type[12]++;
         total_stall += 1;
@@ -1011,9 +726,6 @@ int handle_f(uint32_t instruction, uint32_t rd, uint32_t rs1, uint32_t rs2, uint
     if(func7 == 26){
         float input_val = get_register(rs1);
         result = fcvtsw(input_val);
-        fprintf(log_file, "fcvtsw %.0f = ", input_val);
-        print_float_as_32bit(log_file, result);
-        fprintf(log_file, "\n");
         set_register(rd, result);
         // counter.f_type[13]++;
         total_stall += 1;
@@ -1026,17 +738,9 @@ int handle_f(uint32_t instruction, uint32_t rd, uint32_t rs1, uint32_t rs2, uint
         } else {
             result = a1;
         }
-        fprintf(log_file, "floor %.9f = ", a1);
-        print_float_as_32bit(log_file, result);
-        fprintf(log_file, "\n");
         set_register(rd, result);
         // counter.f_type[14]++;
     }
-
-    
-    print_float_as_32bit(memory_file, result);
-    fprintf(memory_file,"\n");
-
     // rdにresultを格納
     // set_register(rd, result);
     return 1;
@@ -1181,10 +885,11 @@ long long total_cycles;
 
 void print_execution_time_prediction() {
 
-    long long total_instruction = 29621244993;
-    total_stall = 5172166209;
-    total_accesses = 11247101617;
-    cache_misses = 59296145; //3*75
+    // 256*256
+    // long long total_instruction = 29621244993;
+    // total_stall = 5172166209;
+    // total_accesses = 11247101617;
+    // cache_misses = 59296145; //3*75
 
     // クロックサイクル時間（ナノ秒）
     double clock_cycle_time_ns = 1.0 / (cpu_frequency * 1.0e9) * 1.0e9;
